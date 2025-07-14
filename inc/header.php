33dc9fbb5b3d879a291cc9fa07c4eb4b1e648e9b
Harena <harenaryu@gmail.com>
2025-07-14 11:27:49 +0300

    ETU003958

--- inc/header.php
+++ inc/header.php
@@ -1,31 +1,41 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
+if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 ?>
-<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="/accueil.php">MonSite</a>
+<link rel="stylesheet" href="../assets/css/header.css">
+<link rel="stylesheet" href="../assets/bootstrap-icons/bootstrap-icons.css">
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal">
+<nav class="navbar navbar-expand-lg navbar-custom shadow-sm">
+    <div class="container">
+        <a class="navbar-brand text-white fw-bold" href="../pages/liste_obj.php">
+            <i class="bi bi-house-door-fill me-1"></i> Emprunt-site
+        </a>
+
+        <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="menuPrincipal">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <div class="collapse navbar-collapse justify-content-center" id="menuPrincipal">
+            <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="../pages/liste_obj.php">Liste des objets</a>
+                    <a class="nav-link" href="../pages/liste_objets.php">
+                        <i class="bi bi-box-seam"></i> Objets
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../pages/profil.php">Profil</a>
+                    <a class="nav-link" href="../pages/profil.php">
+                        <i class="bi bi-person-circle"></i> Profil
+                    </a>
                 </li>
-                <li class="nav-item">
-                    <form action="../pages/logout.php" method="post" class="d-inline">
-                        <button class="btn btn-danger btn-sm ms-2" type="submit">Déconnexion</button>
-                    </form>
-                </li>
+                
             </ul>
         </div>
+
+        <form action="/logout.php" method="post" class="d-flex ms-auto">
+            <button type="submit" class="btn-deconnexion">
+                <i class="bi bi-box-arrow-right"></i> Déconnexion
+            </button>
+        </form>
     </div>
 </nav>
